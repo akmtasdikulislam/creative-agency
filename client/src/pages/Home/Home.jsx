f019e90e7cb05f1666dab1634312676de51bb755
Akm Tasdikul Islam <akmtasdikulislam@gmail.com>
2025-03-03 06:31:57 +0600

    Add Home component: Implement main layout with Header

---
+++ client/src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../../components/Home/Header/Header";
+
+const Home = () => {
+  return (
+    <main className="max-w-screen min-h-screen">
+      <Header />
+    </main>
+  );
+};
+
+export default Home;
