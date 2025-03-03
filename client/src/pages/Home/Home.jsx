2dfaf456a2332d2eca3ec063170c8c5c344f9ee5
Akm Tasdikul Islam <akmtasdikulislam@gmail.com>
2025-03-03 16:11:22 +0600

    Add TrustedPartners component to Home: Include section for displaying trusted partners

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../../components/Home/Header/Header";
+import TrustedPartners from "../../components/TrustedPartners/TrustedPartners";
 
 const Home = () => {
   return (
     <main className="max-w-screen min-h-screen">
       <Header />
+      <TrustedPartners />
     </main>
   );
 };
