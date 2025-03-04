c6054f7b25f84f5b23144054d3ee94deea390c86
Akm Tasdikul Islam <akmtasdikulislam@gmail.com>
2025-03-04 15:20:35 +0600

    Update ServiceCard component: Add focus and active shadow effects for improved accessibility

--- client/src/components/Home/Services/ServiceCard.jsx
+++ client/src/components/Home/Services/ServiceCard.jsx
@@ -3,10 +3,7 @@ import React from "react";
 const ServiceCard = ({ service }) => {
   const { title, description, imageUrl } = service;
   return (
-    <div
-      className="card bg-base-100 w-full flex-auto shadow-sm transition-all duration-500 hover:cursor-pointer hover:shadow-2xl sm:w-80 md:flex-none"
-      onClick={() => console.log(service)}
-    >
+    <div className="card bg-base-100 w-full flex-auto shadow-sm transition-all duration-500 hover:cursor-pointer hover:shadow-2xl focus:shadow-2xl active:shadow-2xl sm:w-80 md:flex-none">
       <figure className="px-10 pt-10">
         <img src={imageUrl} alt={title} className="h-auto w-20 rounded-full" />
       </figure>
