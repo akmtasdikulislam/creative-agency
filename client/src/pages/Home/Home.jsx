fa7d611717f45b409b7d3f569a10e08486d4f625
Akm Tasdikul Islam <akmtasdikulislam@gmail.com>
2025-03-04 19:07:24 +0600

    Add OurWorks section to Home page: Integrate project portfolio display

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ContactUs from "../../components/Home/ContactUs/ContactUs";
 import Header from "../../components/Home/Header/Header";
+import OurWorks from "../../components/Home/OurWorks/OurWorks";
 import Reviews from "../../components/Home/Reviews/Reviews";
 import Services from "../../components/Home/Services/Services";
 import TrustedPartners from "../../components/TrustedPartners/TrustedPartners";
@@ -11,6 +12,7 @@ const Home = () => {
       <Header />
       <TrustedPartners />
       <Services />
+      <OurWorks />
       <Reviews />
       <ContactUs />
     </main>
