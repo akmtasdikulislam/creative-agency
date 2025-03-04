34411f9efd596b7d48e07395921ba3fdbb483fc9
Akm Tasdikul Islam <akmtasdikulislam@gmail.com>
2025-03-04 06:31:40 +0600

    Update Header component: Change background color class to use primary theme color

--- client/src/components/Home/Header/Header.jsx
+++ client/src/components/Home/Header/Header.jsx
@@ -5,7 +5,7 @@ import HeroSection from "../HeroSection/HeroSection";
 const Header = () => {
   return (
     <div className="relative flex h-screen flex-col items-start">
-      <div className="clip-diagonal absolute -z-10 h-full w-full bg-amber-400"></div>
+      <div className="clip-diagonal bg-primary absolute -z-10 h-full w-full"></div>
       <Navbar />
       <HeroSection />
     </div>
