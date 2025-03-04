59917a46bc52670eaca43802c1b96cc80c357c79
Akm Tasdikul Islam <akmtasdikulislam@gmail.com>
2025-03-04 17:41:25 +0600

    Adjust Services layout: Reduce gap and padding for tighter responsive design

--- client/src/components/Home/Services/Services.jsx
+++ client/src/components/Home/Services/Services.jsx
@@ -61,7 +61,7 @@ const Services = () => {
           View all <HiArrowRight size={""} />
         </Link>
       </div>
-      <div className="xl:px-30 mt-10 flex flex-wrap items-center justify-center gap-10 px-5 md:px-20">
+      <div className="xl:px-30 mt-10 flex flex-wrap items-center justify-center gap-5 px-5 md:px-10">
         {servicesData.map((service) => (
           <ServiceCard key={service.id} service={service} />
         ))}
