c66d46634daa5771508ac3d7d8cb0f362988d7fd
Akm Tasdikul Islam <akmtasdikulislam@gmail.com>
2025-03-04 16:04:59 +0600

    Add ContactUs component: Implement layout for project inquiries with ContactUsForm integration

---
+++ client/src/components/Home/ContactUs/ContactUs.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ContactUsForm from "./ContactUsForm";
+
+const ContactUs = () => {
+  return (
+    <section className="bg-primary xl:px-30 mt-10 flex flex-col justify-between gap-10 px-5 py-10 md:flex-row md:px-10 lg:px-20">
+      <div className="md:1/2 flex-auto xl:max-w-lg">
+        <h2 className="mb-5 text-2xl font-bold md:text-3xl lg:mb-8 lg:text-4xl">
+          Let us handle your project, professionally.
+        </h2>
+        <p className="text-sm">
+          With well written codes, we build amazing apps for all platforms,
+          mobile and web apps in general.
+        </p>
+      </div>
+      <div className="flex-auto md:w-1/2 xl:max-w-lg">
+        <ContactUsForm />
+      </div>
+    </section>
+  );
+};
+
+export default ContactUs;
