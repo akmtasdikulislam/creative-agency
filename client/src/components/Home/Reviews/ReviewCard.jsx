edb7af46e77a6b5b8f968aac763f58b9242dbc43
Akm Tasdikul Islam <akmtasdikulislam@gmail.com>
2025-03-04 15:21:45 +0600

    Add ReviewCard component: Implement review display with error handling for images

---
+++ client/src/components/Home/Reviews/ReviewCard.jsx
@@ -0,0 +1,45 @@
+import PropTypes from "prop-types";
+import React from "react";
+import fallbackAvatar from "../../../assets/images/customers/fallbackAvatar.png";
+const ReviewCard = ({ review }) => {
+  const { name, imageUrl, reviewText, designation, companyName } = review;
+
+  const handleImageError = (e) => {
+    e.target.src = fallbackAvatar;
+  };
+
+  return (
+    <div className="card bg-base-100 w-full flex-auto shadow-sm transition-all duration-500 hover:cursor-pointer hover:shadow-2xl focus:shadow-2xl active:shadow-2xl sm:w-96 md:flex-none">
+      <div className="card-title flex items-center gap-4">
+        <figure className="pl-7 pt-10">
+          <img
+            src={imageUrl}
+            alt={name}
+            className="h-12 w-12 rounded-full"
+            loading="lazy"
+            onError={handleImageError}
+          />
+        </figure>
+        <div className="reviewer pt-10">
+          <p className="text-lg font-bold">{name}</p>
+          <p className="text-sm">
+            {designation}, {companyName}
+          </p>
+        </div>
+      </div>
+      <div className="card-body items-center">
+        <p className="text-neutral-color">{reviewText}</p>
+      </div>
+    </div>
+  );
+};
+ReviewCard.propTypes = {
+  review: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    imageUrl: PropTypes.string.isRequired,
+    reviewText: PropTypes.string.isRequired,
+    designation: PropTypes.string.isRequired,
+    companyName: PropTypes.string.isRequired,
+  }).isRequired,
+};
+export default ReviewCard;
