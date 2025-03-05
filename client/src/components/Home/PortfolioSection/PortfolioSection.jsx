f5832dbe60dbaddb7bb0d9d65e5e6824bd46013a
Akm Tasdikul Islam <akmtasdikulislam@gmail.com>
2025-03-05 06:13:32 +0600

    Create PortfolioSection component: Render portfolio section with title and carousel

--- client/src/components/Home/PortfolioSection/PortfolioSection.jsx
+++ client/src/components/Home/PortfolioSection/PortfolioSection.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import WorkSlider from "./WorkSlider";
+import PortfolioCarousel from "./PortfolioCarousel";
 
-const OurWorks = () => {
+const PortfolioSection = () => {
   return (
     <section className="bg-accent py-16 md:py-20">
       <h2 className="mb-12 text-center text-2xl font-bold text-white md:text-4xl">
         Here are some of <span className="text-secondary">our works</span>
       </h2>
-      <WorkSlider />
+      <PortfolioCarousel />
     </section>
   );
 };
 
-export default OurWorks;
+export default PortfolioSection;
