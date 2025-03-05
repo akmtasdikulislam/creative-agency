45a14a46b46258b5b3624a6df258827287e1f353
Akm Tasdikul Islam <akmtasdikulislam@gmail.com>
2025-03-05 06:17:42 +0600

    Refactor HeroSection: Move component into MainHeader and update layout

---
+++ client/src/components/Home/Header/MainHeader.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "../../shared/Navbar/Navbar";
+import HeroSection from "./HeroSection/HeroSection";
+
+const MainHeader = () => {
+  return (
+    <div className="relative flex max-h-max min-h-screen flex-col items-start overflow-x-hidden">
+      <div className="clip-diagonal bg-primary absolute -z-10 h-full w-full"></div>
+      <Navbar />
+      <HeroSection />
+    </div>
+  );
+};
+
+export default MainHeader;
