534831304a22bf83e36a54b14e0811143a30ed43
Akm Tasdikul Islam <akmtasdikulislam@gmail.com>
2025-03-08 10:50:46 +0600

    Update MainHeader component: Adjust min-height and add transition effects for improved layout

--- client/src/components/sections/Home/MainHeader/MainHeader.jsx
+++ client/src/components/sections/Home/MainHeader/MainHeader.jsx
@@ -1,12 +1,9 @@
-import Navbar from "@components/layout/Navigation/Navbar";
 import HeroSection from "@components/sections/Home/HeroSection/HeroSection";
-import React from "react";
 
 const MainHeader = () => {
   return (
-    <div className="relative flex max-h-max min-h-screen flex-col items-start overflow-x-hidden">
-      <div className="clip-diagonal bg-primary absolute -z-10 h-full w-full"></div>
-      <Navbar />
+    <div className="relative mt-0 max-h-max min-h-[80vh] overflow-x-hidden transition-all duration-500 max-2xl:md:min-h-[55vh]">
+      <div className="clip-diagonal bg-primary absolute -z-10 h-full w-full transition-all duration-500"></div>
       <HeroSection />
     </div>
   );
