7d606b4f8e6e9b175e88cb633491739cee5e8991
Akm Tasdikul Islam <akmtasdikulislam@gmail.com>
2025-03-08 10:51:44 +0600

    Add MainLayout component: Create a layout structure with Navbar and Footer for consistent page design

---
+++ client/src/components/layout/MainLayout/MainLayout.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router";
+import Footer from "../Footer/Footer";
+import Navbar from "../Navigation/Navbar";
+
+const MainLayout = () => {
+  return (
+    <main className="max-w-screen relative min-h-full">
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </main>
+  );
+};
+
+export default MainLayout;
