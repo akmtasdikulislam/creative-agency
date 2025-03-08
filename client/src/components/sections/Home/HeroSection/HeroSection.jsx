de84ea3e9cdf830d474ceab8fea9bcfe7bacb365
Akm Tasdikul Islam <akmtasdikulislam@gmail.com>
2025-03-08 10:50:32 +0600

    Update HeroSection component: Adjust max-width properties for better responsiveness

--- client/src/components/sections/Home/HeroSection/HeroSection.jsx
+++ client/src/components/sections/Home/HeroSection/HeroSection.jsx
@@ -2,7 +2,7 @@ import HeroIllustration from "@assets/images/hero-section/hero-illustration.png"
 
 const HeroSection = () => {
   return (
-    <section className="xl:px-30 my-auto flex h-full w-full flex-col-reverse items-center justify-center gap-5 px-5 md:h-2/3 md:flex-row md:justify-between md:gap-5 md:px-10 lg:my-0 lg:gap-10 lg:px-20">
+    <section className="xl:px-30 my-auto flex h-full w-full flex-col-reverse items-center justify-center gap-5 px-5 md:flex-row md:justify-between md:gap-5 md:px-10 lg:my-0 lg:gap-10 lg:px-20">
       <div className="md:min-w-1/3 xl:max-w-1/2 2xl:max-w-1/3 w-full flex-auto">
         <h1 className="text-4xl font-bold md:text-5xl xl:text-7xl">
           Let's Grow Your Brand To The Next Level
@@ -14,7 +14,7 @@ const HeroSection = () => {
         </p>
         <button className="btn btn-neutral">Hire us</button>
       </div>
-      <div className="xl:max-w-2/4 md:max-w-2/3 h-fit w-full flex-auto 2xl:max-w-4xl">
+      <div className="xl:max-w-2/4 md:max-w-2/3 2xl:max-w-auto h-fit w-full flex-auto 2xl:h-full">
         <img
           className="h-full w-full object-contain"
           src={HeroIllustration}
