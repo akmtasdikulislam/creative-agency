9f45bd1237bad46d9513a6b2c8782ad1eae9dc25
Akm Tasdikul Islam <akmtasdikulislam@gmail.com>
2025-03-08 23:06:41 +0600

    Add authentication routes to Routes configuration

--- client/src/Routes.jsx
+++ client/src/Routes.jsx
@@ -1,4 +1,8 @@
+import AuthLayout from "@components/layout/Auth/AuthLayout";
 import MainLayout from "@components/layout/MainLayout/MainLayout";
+import ForgotPassword from "@pages/Auth/ForgotPassword/ForgotPassword";
+import SignIn from "@pages/Auth/SignIn/SignIn";
+import SignUp from "@pages/Auth/SignUp/SignUp";
 import Home from "@pages/Home/Home";
 import Services from "@pages/Services/Services";
 import { createBrowserRouter } from "react-router";
@@ -15,6 +19,24 @@ const router = createBrowserRouter([
       {
         path: "/services",
         element: <Services />,
+      },
+    ],
+  },
+  {
+    path: "/",
+    element: <AuthLayout />,
+    children: [
+      {
+        path: "/sign-in",
+        element: <SignIn />,
+      },
+      {
+        path: "/sign-up",
+        element: <SignUp />,
+      },
+      {
+        path: "/forgot-password",
+        element: <ForgotPassword />,
       },
     ],
   },
