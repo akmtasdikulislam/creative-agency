d70a3a179641f62ef26e37dc8238064b01270c8a
Akm Tasdikul Islam <akmtasdikulislam@gmail.com>
2025-03-08 06:26:12 +0600

    Add FooterSection component: Create a footer with copyright information

---
+++ client/src/components/sections/Home/FooterSection/FooterSection.jsx
@@ -0,0 +1,9 @@
+const FooterSection = () => {
+  return (
+    <footer className="bg-primary text-neutral-color p-10 text-center">
+      <p>copyright Orange Labs 2020</p>
+    </footer>
+  );
+};
+
+export default FooterSection;
