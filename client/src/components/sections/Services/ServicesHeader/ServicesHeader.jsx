f82bbc15c3e46fdaa1fb8878a910abe4794115d0
Akm Tasdikul Islam <akmtasdikulislam@gmail.com>
2025-03-08 15:34:17 +0600

    Add ServicesHeader component: Create a header section for the services page with styled title

---
+++ client/src/components/sections/Services/ServicesHeader/ServicesHeader.jsx
@@ -0,0 +1,11 @@
+const ServicesHeader = () => {
+  return (
+    <div className="bg-primary pb-15 pt-5 md:pb-20">
+      <h2 className="text-center text-2xl font-bold md:text-4xl">
+        Our <span className="text-secondary">Services</span>
+      </h2>
+    </div>
+  );
+};
+
+export default ServicesHeader;
