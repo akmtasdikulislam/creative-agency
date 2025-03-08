ee54721a231f1199a82ba79c87e298bda5fd33d6
Akm Tasdikul Islam <akmtasdikulislam@gmail.com>
2025-03-08 10:51:26 +0600

    Refactor Home component: Remove FooterSection and adjust layout structure

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,22 +1,20 @@
-import FooterSection from "@/components/sections/Home/FooterSection/FooterSection";
+import MainHeader from "@/components/sections/Home/MainHeader/MainHeader";
 import BrandPartners from "@components/sections/Home/BrandPartners/BrandPartners";
 import ContactSection from "@components/sections/Home/ContactSection/ContactSection";
-import MainHeader from "@components/sections/Home/MainHeader/MainHeader";
 import PortfolioSection from "@components/sections/Home/PortfolioSection/PortfolioSection";
 import ReviewSection from "@components/sections/Home/ReviewSection/ReviewSection";
 import ServiceSection from "@components/sections/Home/ServiceSection/ServiceSection";
 
 const Home = () => {
   return (
-    <main className="max-w-screen min-h-screen">
+    <>
       <MainHeader />
       <BrandPartners />
       <ServiceSection />
       <PortfolioSection />
       <ReviewSection />
       <ContactSection />
-      <FooterSection />
-    </main>
+    </>
   );
 };
 
