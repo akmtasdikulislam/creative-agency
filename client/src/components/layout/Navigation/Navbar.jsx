662807af506191d1f07ee28a4c81f62cf12b7e20
Akm Tasdikul Islam <akmtasdikulislam@gmail.com>
2025-03-08 10:51:07 +0600

    Update Navbar component: Change div to nav element and adjust shadow properties for improved styling

--- client/src/components/layout/Navigation/Navbar.jsx
+++ client/src/components/layout/Navigation/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { HiBars3, HiXMark } from "react-icons/hi2";
 import { NavLink } from "react-router";
 import logo from "/assets/images/logos/logo.png";
@@ -10,14 +10,14 @@ const Navbar = () => {
   };
 
   return (
-    <div className="navbar max-w-screen xl:px-30 relative flex w-screen max-md:items-center max-md:justify-center md:justify-between md:px-10 lg:px-20 lg:py-10">
+    <nav className="navbar max-w-screen xl:px-30 bg-primary relative mb-0 flex w-screen max-md:items-center max-md:justify-center md:justify-between md:px-10 md:py-10 lg:px-20">
       <div className="">
         <a className="btn btn-ghost border-none text-xl hover:border-none hover:bg-transparent hover:shadow-none">
           <img src={logo} alt="Creative Agency" className="w-32" />
         </a>
       </div>
       <div
-        className={`max-md:bg-base-100 max-md:p-15 max-md:shadow-3xl transition-all duration-500 max-md:absolute max-md:top-0 max-md:z-10 max-md:flex max-md:h-screen max-md:w-fit max-md:flex-col max-md:justify-between lg:flex-none ${isOpen ? "right-0" : "-right-full"}`}
+        className={`max-md:bg-base-100 max-md:p-15 transition-all duration-500 max-md:absolute max-md:top-0 max-md:z-10 max-md:flex max-md:h-screen max-md:w-fit max-md:flex-col max-md:justify-between max-md:shadow-2xl lg:flex-none ${isOpen ? "right-0" : "-right-full"}`}
       >
         <ul className="menu max-md:menu-vertical menu-horizontal px-1">
           <li>
@@ -64,7 +64,7 @@ const Navbar = () => {
       >
         <HiBars3 size={"2em"} />
       </button>
-    </div>
+    </nav>
   );
 };
 
