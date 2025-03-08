9b5014921698d278ce8bc58406b4a78267ebc30f
Akm Tasdikul Islam <akmtasdikulislam@gmail.com>
2025-03-08 15:38:36 +0600

    Add title update on Home page: Use updateTitle utility to set document title on component mount

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,11 +1,16 @@
 import MainHeader from "@/components/sections/Home/MainHeader/MainHeader";
+import { updateTitle } from "@/utils/updateTitle";
 import BrandPartners from "@components/sections/Home/BrandPartners/BrandPartners";
 import ContactSection from "@components/sections/Home/ContactSection/ContactSection";
 import PortfolioSection from "@components/sections/Home/PortfolioSection/PortfolioSection";
 import ReviewSection from "@components/sections/Home/ReviewSection/ReviewSection";
 import ServiceSection from "@components/sections/Home/ServiceSection/ServiceSection";
+import { useEffect } from "react";
 
 const Home = () => {
+  useEffect(() => {
+    updateTitle("Home");
+  }, []);
   return (
     <>
       <MainHeader />
