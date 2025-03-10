69c92d2610c8345bbba07aab7cfd333cc33b99a2
Akm Tasdikul Islam <akmtasdikulislam@gmail.com>
2025-03-10 23:39:18 +0600

    Wrap App component with AuthProvider for global authentication state

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
 import "@assets/styles/global.css";
+import { AuthProvider } from "@contexts/AuthContext";
 import { RouterProvider } from "react-router";
 import router from "./Routes";
 
 function App() {
   return (
-    <>
+    <AuthProvider>
       <RouterProvider router={router} />
-    </>
+    </AuthProvider>
   );
 }
 
