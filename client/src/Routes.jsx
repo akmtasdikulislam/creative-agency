c9fb0109bfbfebfbe40bc4bd77616138ae23f264
Akm Tasdikul Islam <akmtasdikulislam@gmail.com>
2025-03-10 23:44:51 +0600

    Add dashboard routes and components to Routes configuration

--- client/src/Routes.jsx
+++ client/src/Routes.jsx
@@ -1,8 +1,14 @@
+import DashboardUsers from "@/pages/Dashboard/DashboardUsers/DashboardUsers";
 import AuthLayout from "@components/layout/Auth/AuthLayout";
+import DashboardLayout from "@components/layout/DashboardLayout/DashboardLayout";
 import MainLayout from "@components/layout/MainLayout/MainLayout";
 import ForgotPassword from "@pages/Auth/ForgotPassword/ForgotPassword";
 import SignIn from "@pages/Auth/SignIn/SignIn";
 import SignUp from "@pages/Auth/SignUp/SignUp";
+import DashboardIndex from "@pages/Dashboard/DashboardIndex/DashboardIndex";
+import DashboardOrders from "@pages/Dashboard/DashboardOrders/DashboardOrders";
+import DashboardReviews from "@pages/Dashboard/DashboardReviews/DashboardReviews";
+import DashboardSettings from "@pages/Dashboard/DashboardSettings/DashboardSettings";
 import Home from "@pages/Home/Home";
 import NotFound from "@pages/NotFound/NotFound";
 import Services from "@pages/Services/Services";
@@ -38,6 +44,33 @@ const router = createBrowserRouter([
       {
         path: "/forgot-password",
         element: <ForgotPassword />,
+      },
+    ],
+  },
+  {
+    path: "/dashboard",
+    element: <DashboardLayout />,
+    children: [
+      {
+        path: "/dashboard",
+        index: true,
+        element: <DashboardIndex />,
+      },
+      {
+        path: "/dashboard/users",
+        element: <DashboardUsers />,
+      },
+      {
+        path: "/dashboard/orders",
+        element: <DashboardOrders />,
+      },
+      {
+        path: "/dashboard/settings",
+        element: <DashboardSettings />,
+      },
+      {
+        path: "/dashboard/reviews",
+        element: <DashboardReviews />,
       },
     ],
   },
