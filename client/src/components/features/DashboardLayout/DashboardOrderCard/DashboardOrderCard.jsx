a58e8eef673f0f50e31806c126ae08de2a9dd2d5
Akm Tasdikul Islam <akmtasdikulislam@gmail.com>
2025-03-13 19:20:11 +0600

    Add modal functionality to DashboardOrderCard for viewing order details

--- client/src/components/features/DashboardLayout/DashboardOrderCard/DashboardOrderCard.jsx
+++ client/src/components/features/DashboardLayout/DashboardOrderCard/DashboardOrderCard.jsx
@@ -1,7 +1,27 @@
+import ModalWrapper from "@/components/common/ModalWrapper/ModalWrapper";
+import { useState } from "react";
+import DashboardOrderModal from "../Modals/DashboardOrderModal/DashboardOrderModal";
+
 const DashboardOrderCard = ({ order }) => {
   const { title, description, imageUrl, status } = order;
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  function openModal(e) {
+    e.stopPropagation(); // Stop event propagation
+    setIsOpen(true);
+  }
+
+  function closeModal(e) {
+    e?.stopPropagation(); // Stop event propagation (optional chaining for keyboard events)
+    setIsOpen(false);
+  }
+
   return (
-    <div className="border-1 border-accent-200 max-h-70 sm:min-w-xs flex flex-auto flex-col gap-5 overflow-hidden rounded-lg bg-white p-5 shadow-sm transition-all duration-500 hover:cursor-pointer hover:shadow-lg focus:shadow-lg active:shadow-lg sm:max-w-md sm:flex-1">
+    <div
+      className="border-1 border-accent-200 max-h-70 sm:min-w-xs flex flex-auto flex-col gap-5 overflow-hidden rounded-lg bg-white p-5 shadow-sm transition-all duration-500 hover:cursor-pointer hover:shadow-lg focus:shadow-lg active:shadow-lg sm:max-w-md sm:flex-1"
+      onClick={openModal}
+    >
       <div className="flex w-full gap-5">
         <div className="service-image">
           <img
@@ -25,6 +45,9 @@ const DashboardOrderCard = ({ order }) => {
           {description}
         </p>
       </div>
+      <ModalWrapper isOpen={isOpen} onClose={closeModal}>
+        <DashboardOrderModal order={order} onClose={closeModal} />
+      </ModalWrapper>
     </div>
   );
 };
