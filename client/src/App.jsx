d58507af7a8de6d40ca48788eb1147c03ba8d291
Akm Tasdikul Islam <akmtasdikulislam@gmail.com>
2025-03-13 19:22:14 +0600

    Add Toaster component for notification management in App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import "@assets/styles/global.css";
 import { AuthProvider } from "@contexts/AuthContext";
+import { Toaster } from "react-hot-toast";
 import { RouterProvider } from "react-router";
 import router from "./Routes";
 
@@ -7,6 +8,12 @@ function App() {
   return (
     <AuthProvider>
       <RouterProvider router={router} />
+      <Toaster
+        position="top-right"
+        toastOptions={{
+          className: "!font-normal !text-sm !max-w-md !w-auto",
+        }}
+      />
     </AuthProvider>
   );
 }
