a06317129081df55a0b2760520a19b481d46c9b0
Akm Tasdikul Islam <akmtasdikulislam@gmail.com>
2025-03-22 18:01:41 +0600

    Wrap application in ModalContextProvider and integrate ModalController for improved modal management

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,7 @@
 import "@assets/styles/global.css";
+import ModalController from "@components/common/ModalController/ModalController";
 import { AuthProvider } from "@contexts/AuthContext";
+import { ModalContextProvider } from "@contexts/ModalContext ";
 import { Toaster } from "react-hot-toast";
 import { RouterProvider } from "react-router";
 import router from "./Routes";
@@ -7,13 +9,16 @@ import router from "./Routes";
 function App() {
   return (
     <AuthProvider>
-      <RouterProvider router={router} />
-      <Toaster
-        position="top-right"
-        toastOptions={{
-          className: "!font-normal !text-sm !max-w-md !w-auto",
-        }}
-      />
+      <ModalContextProvider>
+        <RouterProvider router={router} />
+        <Toaster
+          position="top-right"
+          toastOptions={{
+            className: "!font-normal !text-sm !max-w-md !w-auto",
+          }}
+        />
+        <ModalController />
+      </ModalContextProvider>
     </AuthProvider>
   );
 }
