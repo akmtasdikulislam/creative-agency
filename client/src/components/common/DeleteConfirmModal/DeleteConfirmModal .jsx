17d5c4e7f674bbc8d7b0de39e6dacfdf9abe3afc
Akm Tasdikul Islam <akmtasdikulislam@gmail.com>
2025-03-22 18:01:26 +0600

    Enhance DeleteConfirmModal to include customizable message and alert for irreversible actions

--- client/src/components/common/DeleteConfirmModal/DeleteConfirmModal .jsx
+++ client/src/components/common/DeleteConfirmModal/DeleteConfirmModal .jsx
@@ -1,15 +1,29 @@
-const DeleteConfirmModal = ({ onConfirm, onCancel }) => {
+import { HiExclamationCircle } from "react-icons/hi2";
+
+const DeleteConfirmModal = ({ message, onConfirm, onClose, onCancel }) => {
+  const handleConfrim = () => {
+    onConfirm();
+    onClose();
+  };
   return (
     <div className="w-sm overflow-hidden">
       <h4 className="text-text-primary border-b-accent-200 border-b-1 -mt-2 mb-4 w-full pb-2 font-bold">
         Confirm Deletion
       </h4>
       <p className="mb-6">
-        Are you sure you want to delete this service? This action cannot be
-        undone.
+        {message || "Are you sure you want to delete this service?"}
       </p>
+      <div
+        role="alert"
+        className="alert alert-error alert-soft mb-4 mt-6 w-full py-2 text-xs"
+      >
+        <span className="flex items-center gap-2">
+          <HiExclamationCircle className="h-full w-5" /> This action cannot be
+          undone!
+        </span>
+      </div>
       <div className="flex justify-between">
-        <button className="tw-btn tw-btn-destructive" onClick={onConfirm}>
+        <button className="tw-btn tw-btn-destructive" onClick={handleConfrim}>
           Confirm Delete
         </button>
         <button className="tw-btn tw-btn-cancel" onClick={onCancel}>
