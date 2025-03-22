9759d5a05b7c6179a79c4605c8d7689f842c2a04
Akm Tasdikul Islam <akmtasdikulislam@gmail.com>
2025-03-22 18:00:59 +0600

    Refactor DashboardServiceCard to use useModal for opening service modal

--- client/src/components/features/DashboardLayout/DashboardServiceCard/DashboardServiceCard.jsx
+++ client/src/components/features/DashboardLayout/DashboardServiceCard/DashboardServiceCard.jsx
@@ -1,42 +1,24 @@
-import DeleteConfirmModal from "@/components/common/DeleteConfirmModal/DeleteConfirmModal ";
-import ModalWrapper from "@/components/common/ModalWrapper/ModalWrapper";
-import { useState } from "react";
-import DashboardServiceModal from "../Modals/DashboardServiceModal/DashboardServiceModal";
+import { MODAL_TYPES } from "@/lib/Modal/ModalRegistry";
+import { useModal } from "@hooks/useModal";
 
 const DashboardServiceCard = ({ service }) => {
-  const [isServiceModalOpen, setIsServiceModalOpen] = useState(false);
-  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
-
-  function openServiceModal(e) {
-    e.stopPropagation(); // Stop event propagation
-    setIsServiceModalOpen(true);
-  }
-
-  function closeServiceModal(e) {
-    e?.stopPropagation(); // Stop event propagation (optional chaining for keyboard events)
-    setIsServiceModalOpen(false);
-  }
-
-  const handleDeleteClick = () => {
-    setIsServiceModalOpen(false);
-    setIsDeleteModalOpen(true);
-  };
-
-  const handleDeleteConfirm = () => {
-    // Handle delete logic here
-    setIsDeleteModalOpen(false);
-  };
-
-  const handleDeleteCancel = () => {
-    setIsDeleteModalOpen(false);
-    setIsServiceModalOpen(true);
-  };
-
   const { title, description, imageUrl, status } = service;
+
+  const { openModal } = useModal();
+
+  const handleCardClick = () => {
+    openModal(`${MODAL_TYPES.SERVICE_MODAL}:${service.id}`, {
+      service,
+      onSave: (updatedService) => {
+        console.log(updatedService);
+      },
+    });
+  };
+
   return (
     <div
       className="card bg-base-100 border-1 border-accent-200 sm:min-w-xs w-full flex-auto shadow-sm transition-all duration-500 hover:cursor-pointer hover:shadow-lg focus:shadow-lg active:shadow-lg sm:max-w-md sm:flex-1"
-      onClick={openServiceModal}
+      onClick={handleCardClick}
     >
       <div className="flex items-start justify-between px-7 pt-6">
         <img src={imageUrl} alt={title} className="w-15 h-auto rounded-full" />
@@ -48,20 +30,6 @@ const DashboardServiceCard = ({ service }) => {
           {description}
         </p>
       </div>
-      <ModalWrapper isOpen={isServiceModalOpen} onClose={closeServiceModal}>
-        <DashboardServiceModal
-          service={service}
-          onClose={closeServiceModal}
-          onDelete={handleDeleteClick}
-        />
-      </ModalWrapper>
-
-      <ModalWrapper isOpen={isDeleteModalOpen} onClose={handleDeleteCancel}>
-        <DeleteConfirmModal
-          onConfirm={handleDeleteConfirm}
-          onCancel={handleDeleteCancel}
-        />
-      </ModalWrapper>
     </div>
   );
 };
